fa33e6da8ccfaf2f8038f6034f8b9ab97b08591b
ismaelsilva123 <ismael.silva@escolar.ifrn.edu.br>
2024-08-28 15:23:37 -0300

    movimentooo

--- My project (3)/Assets/scripts 1/player.cs
+++ My project (3)/Assets/scripts 1/player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
+public float Speed;
+
+// Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+    
+  void Move() 
+  {
+      Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+      transform.position += movement * Time.deltaTime * Speed;
+  }
 }
+  
